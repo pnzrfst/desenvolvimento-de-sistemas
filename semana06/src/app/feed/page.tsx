d50e14367678d4a6e7714542525698fba52e9419
gustavoroberto1 <gustavo.roberto@edu.sc.senai.br>
2025-03-13 22:02:48 -0300

    feat

--- semana06/src/app/feed/page.tsx
+++ semana06/src/app/feed/page.tsx
@@ -1,5 +1,9 @@
 import Header from "@/components/Header";
 import "./styles.css"
+import Image from "next/image";
+
+import cover from '@/assets/cover.avif';
+import Avatar from "@/components/Avatar";
 
 export default function Feed() {
     return (
@@ -7,6 +11,13 @@ export default function Feed() {
             <Header />
             <div className="container">
                 <aside className="sidebar">
+                    <Image src={cover} alt="cover" className="cover" />
+
+                    <div className="profile">
+                        <Avatar src="https://github.com/gusttavosouza.png" hasBorder />
+                        <strong>Gustavo Souza</strong>
+                        <span>Full-Stack Developer</span>
+                    </div>
 
                 </aside>
 
