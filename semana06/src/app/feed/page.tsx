f59d4f02f1a44617d3b2fcc355e582e9020896c0
LUIZ CARLOS PEDRO <luiz_c_pedro@estudante.sc.senai.br>
2025-03-17 20:33:22 -0300

    subindo

--- semana06/src/app/feed/page.tsx
+++ semana06/src/app/feed/page.tsx
@@ -2,6 +2,7 @@
 import Header from '@/components/Header'
 import './styles.css'
 import Avatar from '@/components/Avatar'
+import { MdEdit } from "react-icons/md";
 
 export default function Feed() {
     return (
@@ -11,57 +12,18 @@ export default function Feed() {
             <section className="main-container">
                 <aside className="profile">
                     <div className='profile-infos'>
-                        <Avatar imgUrl="https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?semt=ais_hybrid" hasBorder = {true}/>
+                        <Avatar imgUrl={"https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg"} hasBorder={true} />
                         <h1>Luiz Carlos</h1>
                     </div>
-                    <div className="profile-options">
+                    <footer className="profile-options">
                         <h4 className="about">Estudante de desenvolvimento de sistemas</h4>
-                        <button className="edit-profile">Editar perfil</button>
-                    </div>
+                        <button className="edit-profile"><MdEdit />Editar perfil</button>
+                    </footer>
                 </aside>
 
                 <main className="feed">
-                    <ol className="posts">
-                        <li className="post">
-                            <div className="post-header">
-                                <Avatar imgUrl = {"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?semt=ais_hybrid"} />
-                                <p>Postado há 3min</p>
-                            </div>
-                            <div className="about-post">
-                               <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </li>
-                        <li className="post">
-                            <div className="post-header">
-                                <Avatar imgUrl = {"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?semt=ais_hybrid"}/>
-                                <p>Postado há 3min</p>
-                            </div>
-                            <div className="about-post">
-                               <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </li>
-                        <li className="post">
-                            <div className="post-header">
-                                <Avatar imgUrl = {"https://img.freepik.com/premium-vector/man-avatar-profile-picture-vector-illustration_268834-538.jpg?semt=ais_hybrid"} />
-                                <p>Postado há 3min</p>
-                            </div>
-                            <div className="about-post">
-                               <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                                Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                            </div>
-                        </li>
-                    </ol>
+                    <ul className="posts">
+                    </ul>
                 </main>
             </section>
         </section>
