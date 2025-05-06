737211a338a20ac636ff4ca4bf9bb2c28fc46ef2
Carlos Roberto da Silva Filho <silva@edu.sc.senai.br>
2025-05-05 22:01:32 -0300

    feat

---
+++ semana13/src/controller/TaskController.ts
@@ -0,0 +1,53 @@
+import { FastifyInstance, FastifyReply, FastifyRequest } from "fastify";
+import { taskService } from "../service/TaskService";
+
+export async function taskController(app: FastifyInstance) {
+    app.post("/task", (request, reply) => {
+        const body = request.body as { text: string };
+
+        try {
+            taskService.create(body.text);
+            return reply.code(201).send();
+        } catch (error: any) {
+            return reply.code(409).send({ erro: error.message })
+        }
+    })
+
+    app.get("/task", (_, reply) => {
+        const list = taskService.getAll();
+        return reply.code(200).send(list);
+    })
+
+    app.get("/task/:id", (request: FastifyRequest, reply: FastifyReply) => {
+       const { id } = request.params as { id: string };
+       const task = taskService.getById(id);
+       return task;
+    })
+
+    app.patch("/task/:id/completed", (request, reply) => {
+        // CAPTURA INFORMAÇÃO
+        const { id } = request.params as { id: string };
+        
+        try {
+            // RAPASSA INFO RECEBIDA E RECEBE INFORMAÇÃO PROCESSADA
+            const task = taskService.updateCompleted(id);
+            // RETORNA UMA RESPONSE PARA QUEM CHAMOU A ROTA
+            return reply.code(200).send(task);
+        }catch(error: any) {
+            return reply.code(404).send({ error: error.message})
+        }
+    });
+
+    app.patch("/task/:id/text", (request, reply) => {
+        const { id } = request.params as { id: string };
+        const { text } = request.body as { text: string };
+
+        try {
+            const task = taskService.updateText(id, text);
+            return reply.code(200).send(task);
+        }catch(error: any) {
+            return reply.code(404).send({ error: error.message });
+        }
+    })
+
+}
---
+++ semana13/src/service/TaskService.ts
@@ -0,0 +1,49 @@
+import { Task } from "../entity/Task";
+
+class TaskService {
+
+    private taskList: Task[] = [];
+
+    public create(text: string): void {
+        const textAlreadyExist = this.taskList.find(task => task.getText() === text);
+        if (textAlreadyExist) {
+            throw new Error("Já existe uma tarefa com esse texto.")
+        }
+
+        const newTask = new Task(text);
+        this.taskList.push(newTask);
+    }
+
+    public getAll(): Task[] {
+        return this.taskList;
+    }
+
+    public getById(id: string): Task | null {
+        const task = this.taskList.find(task => task.getId() === id);
+        return task ? task : null;
+    }
+
+    public updateCompleted(id: string){
+        const task = this.getById(id);
+        if(task === null){
+            throw new Error("Tarefa não foi encontrada.")
+        }
+
+        task.setCompleted(); 
+        return task;
+    }
+
+    public updateText(id: string, text: string){
+        const task = this.getById(id);
+        if(task === null){
+            throw new Error("Tarefa não foi encontrada.")
+        }
+
+        task.setText(text);
+        return task;
+    }
+
+}
+
+export const taskService = new TaskService();
+
